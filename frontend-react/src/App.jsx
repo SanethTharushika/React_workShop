19bf7e6c9a2740246fe49b91b48dea7870bb4dac
saneth <sanethsiriwardhana@gmail.com>
2026-06-23 11:43:11 +0530

    frondend using react ❤️❤️

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ProductCard from './components/productCard.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    <ProductCard name="MSI" price="250000" photo="https://picsum.photos/id/1/200/300" />
+
+      
+    </>
+  )
+}
+
+export default App
