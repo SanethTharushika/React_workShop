89cc661b3791a9f8a802d76fedb345d56e5739ca
saneth <sanethsiriwardhana@gmail.com>
2026-07-12 16:12:37 +0530

    React hooks useState , react icons , react toast 🖋️

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,18 +4,26 @@ import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import RegisterPage from './pages/registerPage'
 import AdminPage from './pages/adminPage'
+import TestPage from './pages/testPage'
+
 import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
 
   return (
     <div className="w-full h-full bg-red-500">
+
+      <Toaster position="top-right"/>
+
+
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signin" element={<LoginPage />} />
       <Route path="/signup" element={<RegisterPage />} />
       <Route path="/admin/*" element={<AdminPage />} />
+      <Route path="/test" element={<TestPage />} />
     </Routes> 
     </div>
   )
---
+++ frontend-react/src/pages/testPage.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { FaDocker } from "react-icons/fa";
+
+export default function TestPage() {
+
+    const [score, setScore] = useState(50);
+    const [isFollewd, setIsFollowed] = useState(false);
+
+    // let score = 50;
+
+    return (
+        <div className="w-min-full min-h-screen bg-green-500 flex justify-center items-center">
+            <div className="w-[450px] h-[450px] bg-white flex flex-col justify-center items-center">
+                <h1 className="text-5xl font-bold">{score}</h1>
+                <div className="w-full h-[100px] flex justify-center items-center">
+                    <button className="w-[100px] h-[50px] bg-green-500 text-white rounded-lg mx-5"
+                        onClick={() => {
+                            setScore(score + 1);
+                            toast.success("Score incremented");
+                        }
+                        }>
+                        Increment
+                    </button>
+                    <button className="w-[100px] h-[50px] bg-red-500 text-white rounded-lg mx-5"
+                        onClick={() => {
+                            setScore(score - 1);
+                            toast.error("Score decremented");
+                        }
+                        }>
+                        Decrement
+                    </button>
+                </div>
+                <FaDocker onClick={
+                    () => {
+                        setIsFollowed(!isFollewd);
+                        toast.success( isFollewd ? "Unfollowed" : "Followed",{icon: <FaDocker className={isFollewd ? "text-[20px] text-gray-500" : "text-[20px] text-blue-500"} />});
+                    }
+                } className={isFollewd ? "text-[100px] text-blue-500" : "text-[100px] text-gray-500"} />
+            </div>
+
+        </div>
+    )
+}
