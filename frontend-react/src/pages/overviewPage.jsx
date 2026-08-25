ecfec34c4e3da3667a9d53ec2085fc4f6ff90351
saneth <sanethsiriwardhana@gmail.com>
2026-08-25 22:40:40 +0530

    completed product overview🖋️🖋️

--- frontend-react/src/pages/overviewPage.jsx
+++ frontend-react/src/pages/overviewPage.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import api from "../utils/api.js";
 import LoadingScreen from "../components/loadingScreen.jsx";
 import ProductImageSlideShow from "../components/productImageSlideShow.jsx";
+import { getFormattedPrice } from "../utils/price-formatter.jsx";
 
 
 
@@ -45,8 +46,32 @@ export default function OverviewPage() {
                     
                     </div>
 
-                    <div className="w-1/2 bg-blue-900 h-full">
-                    
+                    <div className="w-1/2  h-full flex flex-col p-6 ">
+                    <span className="text-gray-500 text-sm italic mb-4">Product ID: {product.productId}</span>
+                    <p className="text-gray-500 text-sm italic mb-4">{product.brand+ " " + product.model}</p>
+
+                    <h1 className="text-3xl font-semibold mb-6">{product.name}</h1>
+                        {
+                            product.altNames.map(
+                                (altNames, index) => {
+                                    return (
+                                        <span key={index} className="text-gray-500">{" | " + altNames}</span>
+                                    )
+                                }
+                            )
+                        }
+
+                        {
+                            product.price > product.labelledPrice && <p className="text-gray-500 line-through decoration-2 mb-2 text-lg ">{getFormattedPrice(product.labelledPrice)}</p>
+
+                        }
+                        <p className="text-xl text-accent font-semibold">{getFormattedPrice(product.price)}</p>
+                        <p className="text-gray-700 mt-6">{product.description}</p>
+                        <div className="flex">
+                            <button className="w-[220px] p-2 text-white bg-accent rounded-sm hover:bg-accent/90 mt-6">Add to Cart</button>
+                            <button className="w-[220px] p-2 text-gray-700 bg-gray-300 rounded-sm hover:bg-gray-400 mt-6 ml-4">Buy Now</button>
+
+                        </div>
                     </div>
 
 
