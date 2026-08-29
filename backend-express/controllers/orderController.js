8010e619c5957e22ab71e7550e2f839210da6dfd
saneth <sanethsiriwardhana@gmail.com>
2026-08-29 09:23:01 +0530

    for order complete making order model schema in backend side🖋️

---
+++ backend-express/controllers/orderController.js
@@ -0,0 +1,11 @@
+export async function createOrder(req, res) {
+
+    try {
+        let orderId =
+
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+} 
---
+++ backend-express/models/order.js
@@ -0,0 +1,87 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    items: [
+        {
+            product: {
+                productId: {
+                    type: String,
+                    required: true,
+                },
+                name: {
+                    type: String,
+                    required: true,
+                },
+                image: {
+                    type: String,
+                    required: true,
+                },
+                price: {
+                    type: Number,
+                    required: true,
+                },
+                labelledPrice: {
+                    type: Number,
+                    required: true,
+                },
+
+
+            },
+            email: {
+                type: String,
+                required: true,
+            },
+            firstName: {
+                type: String,
+                required: true,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+            addressLine1: {
+                type: String,
+                required: true,
+            },
+            addressLine2: {
+                type: String,
+                required: false,
+
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+            phone: {
+                type: String,
+                required: true,
+            },
+            status: {
+                type: String,
+                required: true,
+                default: "pending",
+            } ,
+            date: {
+                type: Date,
+                required: true,
+                default: Date.now,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1,
+            }
+        }
+    ]
+  }
+)
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
