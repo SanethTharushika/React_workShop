072b5663815b1018899d4bec0c54276fb25fc8a3
saneth <sanethsiriwardhana@gmail.com>
2026-09-03 10:10:10 +0530

    fix the backend getUser function for the home page profile view🖋️

--- backend-express/controllers/userController.js
+++ backend-express/controllers/userController.js
@@ -84,3 +84,37 @@ export async function loginUser(req, res) {
         res.json({ message: error.message });
     }
 }
+
+export async function getUser(req, res) {
+
+    if(req.user == null) {
+        res.status(401).json({ message: "Unauthorized access..." });
+        return;
+    }
+    try {
+         const user = await User.findOne({ email: req.user.email });
+
+         if(user == null) {
+            res.status(404).json({ message: "User not found..." });
+            return;
+         }
+
+         if(user.isBlocked) {
+            res.status(403).json({ message: "User is blocked..." });
+            return;
+         }
+
+         res.json({
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            isAdmin: user.isAdmin,
+            isBlocked: user.isBlocked,
+            isEmailVerified: user.isEmailVerified,
+            image: user.image
+         });
+
+    }catch (error) {
+        res.json({ message: error.message });
+    } 
+}
--- backend-express/routers/userRouter.js
+++ backend-express/routers/userRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {createUser } from '../controllers/userController.js';
 import { loginUser } from '../controllers/userController.js';
+import { getUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/me", getUser);
 
 export default userRouter;
