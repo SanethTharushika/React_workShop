fcd7af9d0610f7afe92520fdfa35b51a4863839a
saneth <sanethsiriwardhana@gmail.com>
2026-09-18 23:02:24 +0530

    now user enterd the email can get the OTP number 🖋️

--- frontend-react/src/pages/forgetPasswordPage.jsx
+++ frontend-react/src/pages/forgetPasswordPage.jsx
@@ -28,7 +28,7 @@ export default function ForgetPasswordPage() {
             }
             <div className="w-[400px] h-[400px] backdrop-blur-md  shadow-lg rounded-lg flex flex-col justify-center items-center gap-4">
                 <h1 className="text-2xl font-semibold text-white">Enter Your Email</h1>
-                <input type="email" placeholder="user@gmail.com" className="w-[80%] h-10 rounded-md px-2 text-white border border-white" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="user@gmail.com" className="w-[80%] h-10 rounded-md px-2 text-white border border-white" />
                 <button disabled={loading} onClick={sendOTP} className="w-[80%] h-10 rounded-md bg-blue-500 text-white font-semibold hover:bg-blue-600">Submit</button>
             </div>
         </div>
