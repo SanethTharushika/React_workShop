b682bd95e26ccc3201263c055ff78229a9b97afa
saneth <sanethsiriwardhana@gmail.com>
2026-09-21 22:05:53 +0530

    code the UI for search bar🖋️

--- frontend-react/src/pages/productsPage.jsx
+++ frontend-react/src/pages/productsPage.jsx
@@ -4,10 +4,12 @@ import api from "../utils/api.js";
 import LoadingScreen from "../components/loadingScreen.jsx"; 
 import ProductCard from "../components/productCard.jsx";
 
+
 export default function ProductsPage() {
 
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         api.get("/products")
@@ -22,11 +24,26 @@ export default function ProductsPage() {
             });
     }, []);
 
+    function searchProducts() {
+        setLoading(true);
+    }
+
     return (
         <div className="w-full bg-primary flex justify-center items-center gap-6 p-20 flex-wrap">
            {
                 loading && <LoadingScreen/>
            }
+
+           <div className="w-full h-[70px] justify-center items-center flex gap-4">
+
+                <input type="text" placeholder="Search products..." className="w-[400px] h-[40px] rounded-md p-2" value={query} onChange={(e) => setQuery(e.target.value)} />
+                <button className="w-[120px] h-[40px] bg-secondary text-white px-4 py-2 rounded-md" onClick={searchProducts}>Search</button>
+                <button onClick={() => {
+                    setQuery("");
+                    setLoading(true);
+                }} className="w-[120px] h-[40px] bg-secondary text-white px-4 py-2 rounded-md">All Products</button>
+
+           </div>
 
            {
                 !loading && <>
